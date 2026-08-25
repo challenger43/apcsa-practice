1157a3fe9feef74cf6394ea26b9eecbeea232604
challenger43 <isabella.lo28@student.tamdistrict.org>
2026-08-25 20:17:25 +0000

    nextline quirk

--- day4/Main.java
+++ day4/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         System.out.print("You like to eat rice. Type true or false.");
         boolean likesRice;
-         while (!input.hasNextBoolean()) {
+        while (!input.hasNextBoolean()) {
             System.out.println("You need to type true or false.");
             input.next(); 
         }
@@ -19,18 +19,19 @@ public class Main {
         System.out.println("How many hungry people do you have?");
         int numHungryPeople;
         while (!input.hasNextInt()){
-            System.out.println("Give a whole number of people.");
+            System.out.println("Give a whole number of people. In number form, e.g. 5. Do not type 'five.'");
             input.next();
         }
         numHungryPeople = input.nextInt();
+        input.nextLine();
         System.out.println("How many not too hungry people do you have?");
         int numLessHungryPeople;
         while (!input.hasNextInt()){
-            System.out.println("A whole number of people is needed.");
+            System.out.println("A whole number of people is needed. In number form, e.g. 5. Do not type 'five.'");
             input.next();
         }
         numLessHungryPeople = input.nextInt();
-
+        input.nextLine();
         double cupsOfRiceNotHungry = 0.5;
         int cupsOfRiceHungry = 1;
 

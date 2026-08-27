3dc469d5144abefacbb0539e99751741e48f669e
challenger43 <isabella.lo28@student.tamdistrict.org>
2026-08-27 20:49:54 +0000

    day 5 string practice is currently broken huzzah

---
+++ day5/stringPractice.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class StringPractice {
+
+    public static String practice(String input){
+        int split = input.indexOf(" ");
+        int length = input.length();
+        String firstWord = input.substring(0, split);
+        String secondWord = input.substring(split + 1);
+        return firstWord + "," + secondWord + "," + length;
+    }
+    public static String oneSpace(String input){
+        int space1 = input.indexOf(' ');
+        return (space1 != -1 && space1 == input.lastIndexOf(' '));
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String startPhrase = "The evolution stages of the";
+        // String phrase1 = "Wooper Quagsire";
+        // String phrase2 = "Psyduck Golduck";
+        System.out.println("Enter a phrase of two words, separated by a space. Do not include other spaces");
+        while (!input.hasNextLine()){
+            System.out.println("make sure to type something.");
+            input.nextLine();
+        }
+        String tempValue = input.hasNextLine; 
+        while (!input.hasNextLine()|| !tempValue.Contains(" ") || !oneSpace(tempValue.NextLine())){
+            System.out.println("Follow directions.");
+            input.nextLine();
+        }
+
+        String phrase = practice(input.nextLine());
+        String[] parts = phrase.split(",");
+        String firstWord = parts[0];
+        String secondWord = parts[1];
+        String length = parts[2];
+        String summary = startPhrase + firstWord + "line are " + firstWord + " and " + secondWord
+                + ". " + length + " characters";
+        // boolean unchanged = phrase.equals("Wooper Quagsire");
+
+        System.out.println(summary);
+        //System.out.println("Original unchanged: " + unchanged);
+    }
+}

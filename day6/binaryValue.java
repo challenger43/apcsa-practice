5445fe032fe7b75480d0d5d5b026846a3860ec51
challenger43 <isabella.lo28@student.tamdistrict.org>
2026-09-01 20:56:25 +0000

    average broken code

--- day6/binaryValue.java
+++ day6/binaryValue.java
@@ -1,26 +1,56 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Collections; 
+import java.util.Collections;
 
 public class binaryValue {
     public static void main(String[] args) {
-        System.out.print("Please type an integer: ");
+        System.out.print(
+                "Type the current time separated by 2 colons. Each value in between colons should be 1-2 digits. ");
         Scanner input = new Scanner(System.in);
-        while (!input.hasNextInt()) {
-            System.out.println("You need to type an integer.");
+        String pattern = "\\d{1,2}:\\d{1,2}:\\d{1,2}";
+        while (!input.hasNext(pattern)) {
+            System.out.println("Type the current time separated by 2 colons. For example, 02:54:49 ");
             input.next();
         }
-        List<Integer> binary = new ArrayList<>();
-        int integer = input.nextInt();
-        if (integer == 0){
-            binary.add(0);
+        List<List<Integer>> binary = new ArrayList<>();
+        String timeString = input.next();
+        String[] timeParts = timeString.split(":");
+        for (int i = 0; i < 3; i++) {
+            int integer = Integer.parseInt(timeParts[i]);
+            int ogInt = integer;
+            List<Integer> individualBinary = new ArrayList<>();
+
+            if (integer == 0) {
+                individualBinary.add(0);
+            }
+
+            while (integer > 0) {
+                individualBinary.add(integer % 2);
+                integer /= 2;
+            }
+
+            // while (individualBinary.size() < 6) {
+            //     individualBinary.add(0, 0);
+            // }
+
+            Collections.reverse(individualBinary);
+            binary.add(individualBinary);
         }
-        while (integer > 0) {
-            binary.add(integer % 2);
-            integer /= 2;
+
+        StringBuilder sb = new StringBuilder();
+        String result = "";
+        for (List<Integer> individualBinary : binary) {
+            sb.append(individualBinary);
         }
-        Collections.reverse(binary);
-        System.out.println(binary);
+        
+        System.out.print(sb);
     }
+
 }
+// String timeValue = "";
+// if (i==0) timeValue = "hours";
+// else if (i == 1) timeValue = "minutes";
+// else timeValue = "seconds";
+// // System.out.print(binary + " " + timeValue + " is " + ogInt + " in base
+// 10.");

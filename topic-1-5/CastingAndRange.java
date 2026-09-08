832dd5db9d244798e3e7240ba55126a0218ea241
challenger43 <isabella.lo28@student.tamdistrict.org>
2026-09-08 21:16:15 +0000

    finished casting and range

--- topic-1-5/CastingAndRange.java
+++ topic-1-5/CastingAndRange.java
@@ -4,11 +4,11 @@
 //a3: 3.0
 //B1: 8
 //B2: -8
-//B3:
-//B4:
-//C1: 
-//D1: 
-//D2:
+//B3:9
+//B4:-8
+//C1: 6.5
+//D1: 2147483647
+//D2:-2147483648
 
 public class CastingAndRange{
     public static void main(String[] args) {
@@ -25,12 +25,24 @@ public class CastingAndRange{
         //b:
         double positiveValue = 8.9;
         double negativeValue = -8.9;
+        //c: 
+        int value = 6; 
+        double doubleVal = 0.5;
+        double result = value + doubleVal;
         
-       
+       //d: 
+       int maxInt = Integer.MAX_VALUE;
         System.out.println("B1 positive truncation: " + (int) positiveValue);
         System.out.println("B2 negative truncation: " + (int) negativeValue);
         System.out.println("B3 positive rounding: " + (int) (positiveValue + 0.5));
         System.out.println("B4 negative rounding: " + (int) (negativeValue + 0.5));
+        System.out.println("C1 automatic widening: " + result);
+        System.out.println("D1 maximum int: " + maxInt);
+        System.out.println("D2 maximum int plus one: " + (int)(maxInt+1));
     }
 }
-//explanations after testing
+//explanations after testing:
+//casting before division works because by setting one value to a double it will "overide" the integer of the other value and make the whole value a double
+//casting -8.9 makes -8 because it simply gets rid of the entire decimal point, not necessarily rounding to the nearest integer
+//the integer to double conversion (so the integer gets widened)
+// Because there are only 31 bits to store values, adding one gets 100....000 as bits, but that ends up equalling the smallest negative number rather than another increase in positive, as it has ran out of storage to hold any bigger numbers

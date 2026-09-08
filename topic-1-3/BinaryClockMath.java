a9187e5ae74d95371ef98d58758c92e416db7674
challenger43 <isabella.lo28@student.tamdistrict.org>
2026-09-08 01:45:13 +0000

    added comments explaining code

--- topic-1-3/BinaryClockMath.java
+++ topic-1-3/BinaryClockMath.java
@@ -23,7 +23,7 @@
 // }
 
 //day 7 
-import java.util.Scanner;
+import java.util.Scanner; //allows user input from terminal
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
@@ -33,61 +33,61 @@ public class binaryClockMath {
         System.out.print(
                 "Type the current time separated by 2 colons. Each value in between colons should be 1-2 digits. ");
         Scanner input = new Scanner(System.in);
-        String pattern = "\\d{1,2}:\\d{1,2}:\\d{1,2}";
-        while (!input.hasNext(pattern)) {
+        String pattern = "\\d{1,2}:\\d{1,2}:\\d{1,2}"; //regex to ensure no value errors
+        while (!input.hasNext(pattern)) { //validation loop
             System.out.println("Type the current time separated by 2 colons. For example, 02:54:49 ");
             input.next();
         }
-        List<List<Integer>> binary = new ArrayList<>();
-        String timeString = input.next();
-        String[] timeParts = timeString.split(":");
-        for (int i = 0; i < 3; i++) {
-            int integer = Integer.parseInt(timeParts[i]);
-            int ogInt = integer;
-            List<Integer> individualBinary = new ArrayList<>();
+        List<List<Integer>> binary = new ArrayList<>(); //create a list of array lists
+        String timeString = input.next(); //take the first valid input and set it to time string
+        String[] timeParts = timeString.split(":"); //split up timestring into the 3 numerical values given and put it into an array of strings
+        for (int i = 0; i < 3; i++) { //loop over each numerical value of timestring
+            int integer = Integer.parseInt(timeParts[i]); //convert string to an integer
+            int ogInt = integer; //remember the original integer before conversion
+            List<Integer> individualBinary = new ArrayList<>(); //create a "temporary" list 
 
-            if (integer == 0) {
+            if (integer == 0) { //in case it just equals 00 as a case check
                 individualBinary.add(0);
             }
 
             while (integer > 0) {
-                individualBinary.add(integer % 2);
+                individualBinary.add(integer % 2); //binary math stuff
                 integer /= 2;
             }
-            Collections.reverse(individualBinary);
-            while (individualBinary.size() < 6) {
-                individualBinary.add(0, 0);
+            Collections.reverse(individualBinary); //reverse it because it is being pushed in reverse
+            while (individualBinary.size() < 6) { //add 0's to the front to keep it at a 6 bit form to make it easier to read
+                individualBinary.add(0, 0); //index value to add to front
             }
 
-            binary.add(individualBinary);
+            binary.add(individualBinary); // push the created list into the original list of string list 
         }
-        StringBuilder sb = new StringBuilder();
-        int iterations = 0;
-        for (List<Integer> binaryNumber : binary) {
-            for (int digit : binaryNumber) {
-                sb.append(digit);
+        StringBuilder sb = new StringBuilder(); //make a stringbuilder object thingie
+        int iterations = 0; //iteration counter
+        for (List<Integer> binaryNumber : binary) { //for of/each loop equivalent
+            for (int digit : binaryNumber) { //for of loop again
+                sb.append(digit); //add each digit from the subList of binaryNumber to the string
             }
-            if (iterations < 2) {
+            if (iterations < 2) { //only add for the first two, don't add to the last one or else you end up with 03:03:03: with an extra colon which doesn't look good
                 sb.append(":");
             }
-            iterations += 1;
+            iterations++; //next iteration
         }
-        // checking
-        int iteration = 0;
-        StringBuilder recalculate = new StringBuilder();
+        // checking aka verifying it and reversing the binary calculation
+        int iteration = 0; //iteration counter for my second loop (difference is iterationS vs iteration)
+        StringBuilder recalculate = new StringBuilder(); //make another stringbuilder object
         for (List<Integer> binaryNumber : binary) {
             int tempSum = 0;
-            for (int i = 5; i >= 0; i--) {
-                double digitToAdd = binaryNumber.get(i) * Math.pow(2, 5 - i);
-                tempSum += digitToAdd;
+            for (int i = 5; i >= 0; i--) { //reverse the binary
+                double digitToAdd = binaryNumber.get(i) * Math.pow(2, 5 - i); //get each integer, then multiply it by 2^(5-current iteration)
+                tempSum += digitToAdd; //add to tempsum
             }
-            String tempSumString = Integer.toString(tempSum);
-            if (tempSumString.length()<2){
+            String tempSumString = Integer.toString(tempSum); //turn tempsum into an integer so you can add it to a string
+            if (tempSumString.length() < 2){ //to keep formatting consistent, if sum is just 3, put a 0 in front of it so it becomes 03:03:03
                 tempSumString = "0" + tempSumString;
             }
-            recalculate.append(tempSumString);
+            recalculate.append(tempSumString); //slap tempSumString(one number value) onto recalculate
             if (iteration < 2) {
-                recalculate.append(":");
+                recalculate.append(":"); //same concept as explained above
             }
             iteration++;
         }

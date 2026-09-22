fb399f0a0d359aa1a547a84a885e52c02348582c
challenger43 <isabella.lo28@student.tamdistrict.org>
2026-09-22 20:00:05 +0000

    receipt 2

---
+++ APClassroomLabs/Receipt2.java
@@ -0,0 +1,21 @@
+package APClassroomLabs;
+public class Receipt2
+{
+   public static void main(String [] args){
+        String hsName = "Redwood HS";
+        double drinkCost = 3.50;
+        double candyCost =1.25;
+        double hotDogCost = 2.75;
+        double hamburgerCost = 3.50;
+      System.out.println("**************************************");
+      System.out.println("*                                    *");
+      System.out.println("*     " + hsName + " Snack Bar           *");
+      System.out.println("*                                    *");
+      System.out.println("*     Drink ..........$" + drinkCost + "          *");                      
+      System.out.println("*     Candy ..........$" + candyCost + "          *");     
+      System.out.println("*     Hot Dog ........$" + hotDogCost + "         *");     
+      System.out.println("*     Hamburger ......$" + hamburgerCost+ "         *");     
+      System.out.println("*                                    *");    
+      System.out.println("**************************************");
+   }
+}
